6de27f57ef53201d5c11ebb29366ca1688b59daa
Tiebosch, J.P. (Pepe) <j.tiebosch@student.ru.nl>
2022-04-18 23:50:56 +0200

    Add soundrestore command

--- lib/mcu_command.dart
+++ lib/mcu_command.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kswcardata/kswcardata.dart';
 
@@ -12,6 +11,7 @@ enum McuCommand {
   HOME_BUTTON,
   REBOOT,
   OPEN_AUX,
+  SOUND_RESTORE,
   NOTHING
 }
 
@@ -23,6 +23,7 @@ extension McuCommandExtension on McuCommand {
     case McuCommand.HOME_BUTTON: return 1;
     case McuCommand.REBOOT: return 1;
     case McuCommand.OPEN_AUX: return 1;
+    case McuCommand.SOUND_RESTORE: return 1;
     case McuCommand.NOTHING: return 0;
   }}
   int get subCommand {switch(this) {
@@ -31,7 +32,17 @@ extension McuCommandExtension on McuCommand {
     case McuCommand.HOME_BUTTON: return 114;
     case McuCommand.REBOOT: return 125;
     case McuCommand.OPEN_AUX: return 605;
+    case McuCommand.SOUND_RESTORE: return 604;
     case McuCommand.NOTHING: return 0;
+  }}
+  String? get arg {switch(this) {
+    case McuCommand.SWITCH_TO_OEM: return null;
+    case McuCommand.TURN_OFF_SCREEN: return null;
+    case McuCommand.HOME_BUTTON: return null;
+    case McuCommand.REBOOT: return null;
+    case McuCommand.OPEN_AUX: return null;
+    case McuCommand.SOUND_RESTORE: return '13';
+    case McuCommand.NOTHING: return null;
   }}
   IconData get icon {switch(this) {
     case McuCommand.SWITCH_TO_OEM: return Icons.directions_car;
@@ -39,10 +50,13 @@ extension McuCommandExtension on McuCommand {
     case McuCommand.HOME_BUTTON: return Icons.home;
     case McuCommand.REBOOT: return Icons.restart_alt;
     case McuCommand.OPEN_AUX: return Icons.settings_input_component;
+    case McuCommand.SOUND_RESTORE: return Icons.volume_up;
     case McuCommand.NOTHING: return Icons.close;
   }}
 
-  String get commandJson => '{"command":'+this.command.toString()+',"subCommand":'+this.subCommand.toString()+'}';
+  String get commandJson => this.arg != null 
+    ? '{"command":'+this.command.toString()+',"subCommand":'+this.subCommand.toString()+'}'
+    : '{"command":'+this.command.toString()+',"subCommand":'+this.subCommand.toString()+',"jsonArg":'+(this.arg??'')+'}';
 
   Future<bool> launch() async {
     if(this != McuCommand.NOTHING) 
